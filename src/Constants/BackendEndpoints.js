62f81b2e2e4ea3a16cc20144c06e7e374ad5f1c0
hardiksingh98 <hardik.singh@turtlemint.com>
2020-12-20 17:19:41 +0530

    refactor on basis of deploy

--- src/Constants/BackendEndpoints.js
+++ src/Constants/BackendEndpoints.js
@@ -1,6 +1,6 @@
 export const login = "/user/login";
 export const register = "/user/register";
-export const baseUrl = 'localhost:9004';
+export const baseUrl = 'http://codeitnow.me';
 export const newsFeedUrl = '/getNewsFeed/Tech';
 export const arraysProblems = '/arrays';
 
